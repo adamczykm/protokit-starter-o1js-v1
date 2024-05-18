4b6173b83dccd26a25788451882167db563b751c
reymom <ramon_gs95@hotmail.com>
2024-05-19 01:49:59 +0200

    verify email

--- apps/web/lib/verify/email.ts
+++ apps/web/lib/verify/email.ts
@@ -1,6 +1,7 @@
 import { Bytes } from "o1js";
 import { Bigint2048 } from "o1js-rsa";
 import { verifyDKIMSignature } from "@zk-email/helpers/dist/dkim/index.js";
+import { Hash, Provable } from "o1js";
 
 type EmailVerifyInputs = {
   headers: Bytes;
@@ -44,27 +45,29 @@ export async function generateInputs(
 interface ParsedEmailInputs {
   paypalAccountId: string;
   usdAmount: string;
-  // Add other fields as needed
+  transactionId: string;
 }
 
 async function parseEmail(rawEmail: string): Promise<ParsedEmailInputs> {
-  // Define regex patterns
-  const paypalIdPattern = /PayPal Account ID:\s*(\S+)/;
-  const usdAmountPattern = /Amount:\s*\$([0-9]+\.[0-9]{2})/;
+  const paypalIdPattern = /From:\s*"(.*?)"\s*<servicio@paypal\.es>/;
+  const usdAmountPattern = /([0-9,]+(\.[0-9]{2})?)\s*[A-Z]{3}/;
+  const transactionIdPattern = /(\b[0-9A-Z]{17}\b)/;
 
-  // Match patterns
   const paypalIdMatch = rawEmail.match(paypalIdPattern);
   const usdAmountMatch = rawEmail.match(usdAmountPattern);
+  const transactionIdMatch = rawEmail.match(transactionIdPattern);
 
-  if (!paypalIdMatch || !usdAmountMatch) {
+  if (!paypalIdMatch || !usdAmountMatch || !transactionIdMatch) {
     throw new Error("Failed to parse email content");
   }
 
   const paypalAccountId = paypalIdMatch[1];
   const usdAmount = usdAmountMatch[1];
+  const transactionId = transactionIdMatch[1];
 
   return {
     paypalAccountId,
     usdAmount,
+    transactionId,
   };
 }
