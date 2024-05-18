8c753dd8e326f9ca68f2c6d0492bc6f45fd45ce0
reymom <ramon_gs95@hotmail.com>
2024-05-18 15:18:48 +0200

    containers

--- apps/web/containers/async-page.tsx
+++ apps/web/containers/async-page.tsx
@@ -1,24 +1,35 @@
 "use client";
-import { Faucet } from "@/components/faucet";
-import { useFaucet } from "@/lib/stores/balances";
+import { useState } from "react";
+import { Order } from "@/components/order";
+import { CommitOrder } from "@/components/commit";
 import { useWalletStore } from "@/lib/stores/wallet";
 
 export default function Home() {
-  const wallet = useWalletStore();
-  const drip = useFaucet();
+    const wallet = useWalletStore();
 
-  return (
-    <div className="mx-auto -mt-32 h-full pt-16">
-      <div className="flex h-full w-full items-center justify-center pt-16">
-        <div className="flex basis-4/12 flex-col items-center justify-center 2xl:basis-3/12">
-          <Faucet
-            wallet={wallet.wallet}
-            onConnectWallet={wallet.connectWallet}
-            onDrip={drip}
-            loading={false}
-          />
+    const [orderLoading, setOrderLoading] = useState(false);
+    const [commitLoading, setCommitLoading] = useState(false);
+
+    return (
+        <div className="mx-auto -mt-32 h-full pt-16">
+            <div className="flex h-full w-full items-center justify-center pt-16">
+                <div className="flex basis-4/12 flex-col items-center justify-center 2xl:basis-3/12">
+                    <Order
+                        wallet={wallet.wallet}
+                        onConnectWallet={wallet.connectWallet}
+                        loading={orderLoading}
+                        setLoading={setOrderLoading}
+                    />
+                </div>
+                <div className="flex basis-4/12 flex-col items-center justify-center 2xl:basis-3/12">
+                    <CommitOrder
+                        wallet={wallet.wallet}
+                        onConnectWallet={wallet.connectWallet}
+                        loading={commitLoading}
+                        setLoading={setCommitLoading}
+                    />
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
